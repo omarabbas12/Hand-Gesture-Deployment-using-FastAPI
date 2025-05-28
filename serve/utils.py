73cf7d8b6f28fbfe76972a80997614e24a6b0bf4
omarabbas12 <omarmmabbas@gmail.com>
2025-05-28 18:00:20 +0300

    Checking Everything works in Procustion branch( Serving and testing)

---
+++ serve/utils.py
@@ -0,0 +1,23 @@
+import joblib
+import pandas as pd
+
+
+from transformer import LandmarkNormalizer, map_labels
+
+# Load model
+model = joblib.load("model.pkl")
+
+# Create transformer instance
+normalizer = LandmarkNormalizer()
+
+# Dummy fit for transformer since it's required (fit on dummy structure)
+dummy = pd.DataFrame(columns=[
+    f"{axis}{i}" for i in range(1, 22) for axis in ['x', 'y', 'z']
+])
+dummy.fillna(0, inplace=True)
+normalizer.fit(dummy)
+
+def preprocess_input(df):
+    df = df.drop(columns=[col for col in df.columns if col.startswith('z')], errors='ignore')
+    df = normalizer.transform(df)
+    return df
